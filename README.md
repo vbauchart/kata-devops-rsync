6723000bd7414555b94b76948437d5efb28fc0ad
Vincent Bauchart <vincent.bauchart@pm.me>
2023-12-11 23:03:16 +0100

    add rotation script with snapshots

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # kata-devops-rsync
 
+## Simple
+
+```
+docker compose up -d --build
+
+docker compose exec prodserver bash
+
+$ rsync -av RH SITE operation@backupserver:./backups/backup_$(date +%F_%T)/
+```
+
+## Snapshots
+
 ```
 docker compose up -d --build
 
 docker exec prodserver bash
-
-$ ssh operation@backupserver
-
-$ rsync -av RH SITE operation@backupserver:./backups/backup_$(date +%F_%T)/
+$ docker compose exec --build prodserver sauvegarde.sh
 ```
--- bin/sauvegarde.sh
+++ bin/sauvegarde.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 CURRENT_DATE="$(date +%F_%T)"
 
+echo "Creating target backup directory"
+ssh operation@backupserver mkdir "./backups/${CURRENT_DATE}"
+
 for source_dir in RH SITE;
 do
-    rsync -av "/home/simulation/${source_dir}" "operation@backupserver:./backups/${CURRENT_DATE}/" --link-dest=../latest
+    echo "backup ${source_dir}"
+    rsync -av --delete "/home/simulation/${source_dir}" "operation@backupserver:./backups/${CURRENT_DATE}/" --link-dest="../latest/" -i
 done
+
+echo "Rotate latest"
+ssh operation@backupserver rm -f ./backups/latest
+ssh operation@backupserver ln -sr "./backups/${CURRENT_DATE}/" ./backups/latest
